952d6e28b78bf122be7a2d551036ed3bb6e1db54
Derek Brigner <derek.brigner@ustrotting.com>
2013-05-28 12:38:16 -0400

    update error checking and ajax pointer

--- js/main.js
+++ js/main.js
@@ -7,17 +7,18 @@ $(document).ready(function(){
     var output = $('#output');
 
     $.ajax({
-        url: 'http://lifeafterracing.ustrotting.com/Employees.cfm',
+        url: 'http://lifeafterracing.ustrotting.com/landmarks.cfm',
         dataType: 'jsonp',
         jsonp: 'jsoncallback',
         timeout: 5000,
         success: function(data, status){
             $.each(data, function(i,item){ 
-                var name = '<h1>'+item.firstName+' '+item.lastName+'</h1>';
-                var title = '<h2>'+item.title+'</h2>';
-                
-                output.append(name+title);
-            });
+				var landmark = '<h1>'+item.name+'</h1>'
+				+ '<p>'+item.latitude+'<br>'
+				+ item.longitude+'</p>';
+
+				output.append(landmark);
+			});
         },
         error: function(d, s, e){
            output.text(d.toString()+' '+s.toString()+' '+e.toString());
